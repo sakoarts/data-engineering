6a0bb2bf08b835d7c8ad2fa5dedfc8033682cd15
Zjoszjos <jwamangnus@gmail.com>
2016-03-22 11:24:05 +0100

    CommunityDetection Errorless

--- quickstart/src/main/java/dataengineering12/quickstart/DBLPCommunityDetection.java
+++ quickstart/src/main/java/dataengineering12/quickstart/DBLPCommunityDetection.java
@@ -5,10 +5,7 @@
  */
 package dataengineering12.quickstart;
 
-import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.*;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.util.Collector;
 import org.apache.flink.graph.Edge;
 import org.apache.flink.graph.Vertex;
 import java.io.BufferedReader;
@@ -17,16 +14,19 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.graph.Graph;
+import org.apache.flink.graph.library.CommunityDetection;
 import org.apache.flink.types.NullValue;
 
 /**
  *
  * @author Jos
  */
-public class WordCount {
+public class DBLPCommunityDetection {
+
     private static List<Edge<String, Double>> edges = new ArrayList();
-    private static List<Vertex<String, Long>> vertices= new ArrayList();    
+    private static List<Vertex<String, Long>> vertices = new ArrayList();
 
     public static void input() throws IOException {
         String csvFile = "C:/Users/Jos/Documents/Data Engineering/edges.csv";
@@ -35,11 +35,13 @@ public class WordCount {
         String cvsSplitBy = ";";
         try {
             br = new BufferedReader(new FileReader(csvFile));
-            while ((line = br.readLine()) != null) {
+            int c = 0;
+            while ((line = br.readLine()) != null && c <= 1000000) {
                 // use comma as separator
                 //System.out.println("Line to read: " + line);
                 String[] toProcess = line.split(cvsSplitBy);
                 processLine(toProcess);
+                c++;
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -63,23 +65,34 @@ public class WordCount {
         input();
         System.out.println("CSV is in memory");
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-        System.out.println("Number of Edges in Array: "+ edges.size());
+        System.out.println("Number of Edges in Array: " + edges.size());
         DataSet<Edge<String, Double>> dEdges = env.fromCollection(edges);
         //DataSet<Vertex<String, Long>> dVertices = env.fromCollection(vertices);
-        
-        Graph<String, NullValue, Double> graph;
+
+        Graph<String, NullValue, Double> tgraph;
         System.out.println("Loading Graph from Memory");
-        graph = Graph.fromDataSet(dEdges, env);
-        System.out.println("Computing number of edges");
+        tgraph = Graph.fromDataSet(dEdges, env);
+        DataSet<Vertex<String, Long>> finVertices = tgraph.getVertices().map(new MapFunction<Vertex<String,NullValue>, Vertex<String,Long>>() {
+            @Override
+            public Vertex<String, Long> map(Vertex<String, NullValue> t) {
+                return new Vertex(t.f0, 0l);
+            }
+        });
+        Graph<String, Long, Double> graph;
+        graph = Graph.fromDataSet(finVertices, dEdges, env);
+        //System.out.println("Computing number of edges");
         //System.out.println("Number of Edges: " + graph.numberOfEdges());
-        System.out.println("Number of Vertices: "+ graph.getVertices().count());
+        System.out.println("Number of Vertices: " + graph.getVertices().count());
         System.out.println("INGELADEN");
+        Graph<String, Long, Double> graphWithCommunities;
+        graphWithCommunities = graph.run(new CommunityDetection<String>(20, 0.8));
+
     }
 
-    public static void processLine(String[] s){
-        if(s.length > 1){
-            edges.add(new Edge(s[0], s[1], 0.0));
+    public static void processLine(String[] s) {
+        if (s.length > 1) {
+            edges.add(new Edge(s[0], s[1], Double.valueOf(edges.size())));
         }
     }
-    
+
 }
